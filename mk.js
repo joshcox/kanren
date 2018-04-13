1914078c8a565f5c45a87305bc4a1731ea537abf
Josh Cox <Josh.Cox@cerner.com>
2018-04-13 18:53:25 -0500

    Update: Use ternaries in append$ and appendMap$ for conciseness

--- mk.js
+++ mk.js
@@ -5,27 +5,20 @@ const head = ([car]) => car;
 const tail = ([car, ...cdr]) => cdr;
 
 // Stream
-const append$ = ($1, $2) => {
-    if ($1 instanceof Function) {
-        return () => append$($2, $1());
-    } else if (Array.isArray($1) && $1.length === 0) {
-        return $2;
-    } else {
-        return [head($1), ...append$(tail($1), $2)];
-    }
-};
-const appendMap$ = (g, $) => {
-    if ($ instanceof Function) {
-        return () => appendMap$(g, $());
-    } else if (Array.isArray($) && $.length === 0) {
-        return [];
-    } else {
-        return append$(appendMap$(g, tail($)), g(head($)));
-    }
-};
+const append$ = ($1, $2) => $1 instanceof Function
+    ? () => append$($2, $1())
+    : Array.isArray($1) && $1.length === 0
+        ? $2
+        : [head($1), ...append$(tail($1), $2)];
+
+const appendMap$ = (g, $) => $ instanceof Function
+    ? () => appendMap$(g, $())
+    :Array.isArray($) && $.length === 0
+        ? []
+        : append$(appendMap$(g, tail($)), g(head($)));
 
 // Substitution Store
-const substitution = (left, right) => ({ left, right });
+const substitution = (left, right) => ({left, right});
 const substitutionStore = (left, right, store) => [substitution(left, right), ...store];
 const walkSubstitution = (u, store) => {
     const pr = typeof u === "symbol" && store.find(({left}) => left === u);
@@ -50,17 +43,17 @@ const unification = (u, v, store) => {
 };
 
 // TODO - Make constraints extendable to add any number of stores
-const constraints = (subStore, count) => ({ subStore, count });
+const constraints = (subStore, count) => ({subStore, count});
 
 // unify goal
-const unify = (u, v) => ({ subStore, count }) => {
+const unify = (u, v) => ({subStore, count}) => {
     const s = unification(u, v, subStore);
     return s ? [constraints(s, count)] : [];
 };
 
 // fresh logic variable goal
-const callWithFresh = f => ({ subStore, count}) =>
-    f(Symbol(count))(constraints(subStore, count+1));
+const callWithFresh = f => ({subStore, count}) =>
+    f(Symbol(count))(constraints(subStore, count + 1));
 
 // disjunction/or goal
 const disj = (g1, g2) => constraints =>
@@ -83,16 +76,16 @@ const foo = callWithEmptyState(
 );
 
 const prettyPrint = ([...solutions]) => solutions
-    .map(({ subStore, count }, solutionIndex) => {
+    .map(({subStore, count}, solutionIndex) => {
         return {
             solutionNumber: solutionIndex + 1,
             substitution: subStore
-                .map(({ left, right }) => `${left.toString()}: ${right}`)
+                .map(({left, right}) => `${left.toString()}: ${right}`)
                 .join("\n\t\t"),
             count: `${count}`
         }
     })
-    .forEach(({ solutionNumber, substitution, count }) => {
+    .forEach(({solutionNumber, substitution, count}) => {
         console.log(`Solution #${solutionNumber}:`);
         console.log(`\tNumber of LVars: ${count}`);
         console.log(`\tSubstitution Store:\n\t\t${substitution}`);
