b11b085c63aeef33cbd2e92fef30c911f1a68724
Josh Cox <cox.josh.d@gmail.com>
2018-04-14 19:40:17 -0500

    Document array util module

--- src/util/array.js
+++ src/util/array.js
@@ -1,4 +1,17 @@
+/**
+ * Get the first element of an array
+ * @function head
+ * @param {Array}
+ * @returns {*} the first element
+ */
 const head = ([head]) => head;
+
+/**
+ * Get every element but the first of an array
+ * @function tail
+ * @param {Array}
+ * @returns {Array}
+ */
 const tail = ([head, ...tail]) => tail;
 
 module.exports = {
